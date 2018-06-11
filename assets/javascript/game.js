c48321e317dff3786a649979865b5ce972d28426
Cesar McFarlin <camcfarlin@me.com>
2018-06-10 23:08:26 -0600

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,32 @@
-
-
 // Array of words 
 // var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado','Connecticut', 'Delaware', 'Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington', 'West Virginia','Wisconsin','Wyoming'];
 // Attempted to do states, can not get it to recoginze spaces or duplicate letters
 
-var food = ['turkey', 'bacon', 'ramen', 'pho', 'rice', 'corn', 'bread', 'scone', 'ocra', 'pear', 'beans' ]
+var food = ['turkey', 'bacon', 'ramen', 'pho', 'rice', 'corn', 'bread', 'scone', 'ocra', 'pear', 'beans', 'peas']
 
 // Choose words radomly
-
 var underScore = [];
 var rightGuess = [];
 var wrongGuess = [];
 var incCounter = 10;
 var winScore = 0;
-
-
-
 var randNum = Math.floor(Math.random() * food.length);
 var randWord = food[randNum];
     randWord = randWord.toLowerCase();
-  
+
+
 function newGame() {
-    incCounter = 10;
-    $( ".underscore" ).text ( "_ _ _ _");
-    $( ".lives" ).text( incCounter );  
     underScore = [];
     rightGuess = [];
     wrongGuess = [];
+    incCounter = 10;
+    $( ".underscore" ).text ( "_ _ _ _");
+    $( ".lives" ).text( incCounter );  
     randNum = Math.floor(Math.random() * food.length);
     randWord = food[randNum];
     startOver()
 }
 
-
-startOver ()
-
-function startOver () {
-    
 // Dom mani - replace whats in each repsective div
 
 document.getElementsByClassName('underScore');
@@ -48,7 +38,6 @@ console.log(randWord);
 
 // Creat underscore based on lenght of word
 
-
 var blankUnderscore = () => {
     for(var i = 0; i < randWord.length; i++) {
         underScore.push('_');
@@ -56,7 +45,7 @@ var blankUnderscore = () => {
     return underScore;
 }
 console.log(blankUnderscore());
-// Test
+ 
 //  user input - guess
     
 document.addEventListener('keypress', (event) => {
@@ -77,32 +66,65 @@ document.addEventListener('keypress', (event) => {
         underScore[randWord.indexOf(keyWord)] = keyWord;
         docUnderScore[0].innerHTML = underScore.join(' ');
         docrightGuess[0].innerHTML = rightGuess;
+        
 
 //guess matches all +winscore/alert/startover
 
         if(underScore.join('') == randWord) {
-            
+            winScore ++;
+            $( ".wins" ).text( winScore );
+            alert('You Win');
+            newGame(); 
+        }
+    } 
+
+// If wrong push to wrong array - log onto doc
+    else {wrongGuess.push(keyWord);
+        docWrongGuess[0].innerHTML = wrongGuess;
+        incCounter --;
+        $( ".lives" ).text( incCounter );
+        if(incCounter === 0) {
+            alert("You Lose");
+            newGame();    
+        }
+     };
+    });
+
+//startover without triggering anothe eventlister 
+function startOver () {
+document.getElementsByClassName('underScore');
+var docUnderScore = document.getElementsByClassName('underscore');
+var docrightGuess = document.getElementsByClassName('rightGuess');
+var docWrongGuess = document.getElementsByClassName('wrongGuess');
+console.log(randWord);
+var blankUnderscore = () => {
+    for(var i = 0; i < randWord.length; i++) {
+        underScore.push('_');
+    }
+    return underScore;
+}
+console.log(blankUnderscore());
+    var keyWord = String.fromCharCode(event.keyCode);
+    if (randWord.indexOf(keyWord) > -1) {
+        rightGuess.push(keyWord);
+        underScore[randWord.indexOf(keyWord)] = keyWord;
+        docUnderScore[0].innerHTML = underScore.join(' ');
+        docrightGuess[0].innerHTML = rightGuess;
+        if(underScore.join('') == randWord) {
             winScore ++;
             console.log (winScore);
             $( ".wins" ).text( winScore );
             alert('You Win');
             newGame(); 
-
         }
-
     } 
-
-// If wrong push to wrong array - log onto doc
     else {wrongGuess.push(keyWord);
         docWrongGuess[0].innerHTML = wrongGuess;
         incCounter += -1;
         $( ".lives" ).text( incCounter );
         if(incCounter === 0) {
             alert("You Lose");
-            newGame(); 
-            
+            newGame();    
         }
      };
-    });
-
-}
+    ;}
